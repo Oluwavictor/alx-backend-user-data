c60509e57ee3d8d7df6ddfcdf716755eadd2a4c0
Oluwavictor <46841607+Oluwavictor@users.noreply.github.com>
2023-08-16 22:21:23 +0100

    new commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,8 +9,8 @@ import logging
 from typing import List
 import mysql.connector
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 def filter_datum(
         fields: List[str], redaction: str, message: str,
